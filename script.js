cacf3f7418588c17867b57714deea9c8f8ec67e1
Ezioness <romcas@gmx.fr>
2024-07-27 12:59:12 +0200

    Added hover effect and custom grid size.
    Had to refactor the grid creation function to accept a parameter to make things simpler and code clearer.

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
-let createGrid = () => {
-    const gridContainer = document.querySelector("#grid-container");
-    for(let i = 0; i < 16; i++){
+const gridContainer = document.querySelector("#grid-container");
+
+let removeGrid = () => {
+    let rows = document.querySelectorAll(".row");
+    rows.forEach(row => row.remove());
+}
+
+let createGrid = (size) => {
+    if(gridContainer.childNodes.length < 0) {
+        removeGrid();
+    }
+
+    for(let i = 0; i < size; i++){
         const row = document.createElement("div");
         row.classList.add("row")
-        for(let j = 0; j < 16;j++) {
+        for(let j = 0; j < size;j++) {
             const cell = document.createElement("div");
             cell.classList.add("cell")
             row.appendChild(cell);
         }
         gridContainer.appendChild(row);
     }
+
+    addHoverEffect();
 }
 
-createGrid();
+let addHoverEffect = () => {
+    let cells = document.querySelectorAll(".cell");
+    cells.forEach(cell => {
+        cell.addEventListener("mouseover", e => {
+            e.target.classList.add("hovered")
+            console.log("caca");
+        });
+    })
+}
+
+createGrid(16);
+
+let newBtn = document.querySelector("#new-button");
+newBtn.addEventListener("click", () => {
+    removeGrid();
+    let gridSize = +prompt("New grid size ? (1-100)");
+    
+    gridSize = Math.min(100, Math.max(1, gridSize));
+    createGrid(gridSize);
+})
